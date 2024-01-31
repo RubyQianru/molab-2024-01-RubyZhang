ee10f3166e35ae72c67555c9ddb9080298227bd1
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-01-31 16:30:10 -0500

    Clean up code

--- Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
+++ Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
@@ -8,38 +8,47 @@ let renderer = UIGraphicsImageRenderer(size: CGSize(width: 1024, height: 1024))
 
 let image = renderer.image { (context) in
     
+    //
+    // Define colors
+    // Codes belowed simplified by ChatGPT (how to make this code more simplified?)
+    // simplified from my original code
+    
+    let darkGray = UIColor.darkGray
+    let yellow = UIColor(red: 253/255, green: 218/255, blue: 13/255, alpha: 1)
+    let brown = UIColor(red: 110/255, green: 38/255, blue: 14/255, alpha: 1)
+    let black = UIColor.black
+    let blue = UIColor(red: 64/255, green: 224/255, blue: 208/255, alpha: 1)
+    
     UIColor.darkGray.setStroke()
     context.stroke(renderer.format.bounds)
     
     //yellow body
-    UIColor(red: 253/255, green: 218/255, blue: 13/255, alpha: 1).setFill()
+    yellow.setFill()
     context.fill(CGRect(x: 0, y: 0, width: 300, height: 160))
     
     //brown stripes
-    UIColor(red: 110/255, green: 38/255, blue: 14/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 0, y: 0, width: 30, height: 160))
-    
-    UIColor(red: 110/255, green: 38/255, blue: 14/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 50, y: 0, width: 30, height: 160))
-    
-    UIColor(red: 110/255, green: 38/255, blue: 14/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 100, y: 0, width: 30, height: 160))
+    brown.setFill()
+    for i in 0..<3 {
+        context.fill(CGRect(x: i * 50, y: 0, width: 30, height: 160))
+    }
     
     //black eyes
-    UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 140, y: 60, width: 50, height: 80))
+    black.setFill()
     
-    UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 250, y: 60, width: 50, height: 80))
+    for i in [140, 250] {
+        context.fill(CGRect(x: i, y: 60, width: 50, height: 80))
+    }
+    
     
     //blue pupils
-    UIColor(red: 64/255, green: 224/255, blue: 208/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 165, y: 60, width: 25, height: 25))
+    blue.setFill()
     
-    UIColor(red: 64/255, green: 224/255, blue: 208/255, alpha: 1).setFill()
-    context.fill(CGRect(x: 275, y: 60, width: 25, height: 25))
-    
+    for i in [165, 275] {
+        context.fill(CGRect(x: i, y: 60, width: 25, height: 25))
+    }
+
     //brown legs
+    brown.setFill()
 }
 
 image
