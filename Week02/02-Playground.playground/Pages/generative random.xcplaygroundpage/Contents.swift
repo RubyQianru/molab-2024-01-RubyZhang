2de6f989bed7acd5f73d4b929ff3601b6eb8ee57
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-01-31 13:33:47 -0500

    Replace SwfitUI with UIKit

--- Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
+++ Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
@@ -1,28 +1,19 @@
 //: Create an Xcode playground that makes an 1024x1024 image
 
-import SwiftUI
-import PlaygroundSupport
+import UIKit
 
-// Protocol View: 
-// A type that represents part of your app’s user interface
-// and provides modifiers that you use to configure views.
+let renderer = UIGraphicsImageRenderer(size: CGSize(width: 1024, height: 1024))
 
-struct MyView: View {
-    var body: some View {
-        VStack {
-            Image(systemName:  "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello!")
-        }
-        .padding()
-    }
+let image = renderer.image { (context) in
+    
+    UIColor.darkGray.setStroke()
+    context.stroke(renderer.format.bounds)
+    
+    UIColor(red: 158/255, green: 215/255, blue: 245/255, alpha: 1).setFill()
+    context.fill(CGRect(x: 1, y: 1, width: 140, height: 140))
+    
+    UIColor(red: 145/255, green: 211/255, blue: 205/255, alpha: 1).setFill()
+    context.fill(CGRect(x: 60, y: 60, width: 140, height: 140), blendMode: .multiply)
 }
 
-// Sets the view currently being displayed by Xcode on behalf of the playground page.
-// Based on https://github.com/mobilelabclass-itp/02-Icon-Image
-
-PlaygroundPage.current.setLiveView(
-  MyView()
-    .frame(width: 375, height: 812)
-)
+image
