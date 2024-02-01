4448aa40763beb388838d4c5b4a458dbebb1293f
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-01-31 21:06:19 -0500

    finish drawing bee

--- Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
+++ Week02/02-Playground.playground/Pages/generative random.xcplaygroundpage/Contents.swift
@@ -22,21 +22,30 @@ let image = renderer.image { (context) in
     UIColor.darkGray.setStroke()
     context.stroke(renderer.format.bounds)
     
+    //white wings
+    UIColor.white.setStroke()
+    
+    for i in 0...1 {
+        let wingPath = UIBezierPath(rect: CGRect(x: 512+80+120*i, y: 512-30, width: 75, height: 50))
+        wingPath.lineWidth = 10.0
+        wingPath.stroke()
+    }
+    
     //yellow body
     yellow.setFill()
-    context.fill(CGRect(x: 0, y: 0, width: 300, height: 160))
+    context.fill(CGRect(x: 512, y: 512, width: 300, height: 160))
     
     //brown stripes
     brown.setFill()
     for i in 0..<3 {
-        context.fill(CGRect(x: i * 50, y: 0, width: 30, height: 160))
+        context.fill(CGRect(x: i * 50 + 512, y: 512, width: 30, height: 160))
     }
     
     //black eyes
     black.setFill()
     
     for i in [140, 250] {
-        context.fill(CGRect(x: i, y: 60, width: 50, height: 80))
+        context.fill(CGRect(x: i + 512, y: 60 + 512, width: 50, height: 80))
     }
     
     
@@ -44,11 +53,28 @@ let image = renderer.image { (context) in
     blue.setFill()
     
     for i in [165, 275] {
-        context.fill(CGRect(x: i, y: 60, width: 25, height: 25))
+        context.fill(CGRect(x: i + 512, y: 60 + 512, width: 25, height: 25))
     }
 
-    //brown legs
-    brown.setFill()
+    //black legs
+    black.setFill()
+    
+    for i in [145, 255] {
+        context.fill(CGRect(x: i + 512, y: 160 + 512, width: 25, height: 40))
+    }
+    
+    //black brows
+    black.setFill()
+    
+    for i in [145, 255] {
+        context.fill(CGRect(x: i + 512, y: 20 + 512, width: 45, height: 20))
+    }
+    
+    //black pin
+    black.setFill()
+    
+    context.fill(CGRect(x: 512-30, y: 512+80, width: 30, height: 15))
+    
 }
 
 image
