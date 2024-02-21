1040ab450863448eff17c5dfa968fa12c7754960
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-02-20 20:29:32 -0500

    Creat audio player buttons

--- Week04/Test/Test/Player.swift
+++ Week04/Test/Test/Player.swift
@@ -9,6 +9,37 @@ import SwiftUI
 
 struct Player : View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        VStack{
+            playTab()
+        }
     }
+}
+
+//Padding:
+//https://www.hackingwithswift.com/quick-start/swiftui/how-to-control-spacing-around-individual-views-using-padding
+
+struct playTab : View {
+    var body: some View {
+        HStack{
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "backward.fill")
+                    .imageScale(.large)
+            })
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "play.fill")
+                    .imageScale(.large)
+            })
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "forward.fill")
+                    .imageScale(.large)
+            })
+        }
+        .padding(50)
+    }
+}
+
+#Preview {
+    Player()
 }
